e081385a1372b028330471336c2df804de0865eb
jjobJunior <54251905+jjobJunior@users.noreply.github.com>
2024-05-28 18:45:14 -0300

    Criando a validação de autenticação e resgatando o usuario autenticado

---
+++ backend/middlewares/authGuard.js
@@ -0,0 +1,25 @@
+/**
+ * import { findById } from "../models/User";
+ * import { verify } from "jsonwebtoken";
+ */
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const jwtSecret = process.env.JWT_SECRET;
+
+const authGuard = async (req, res, next) => {
+  const authHearder = req.headers["authorization"];
+  const token = authHearder && authHearder.split(" ")[1];
+
+  if (!token) return res.status(401).json({ errors: ["Acesso negado!"] });
+
+  try {
+    const verified = jwt.verify(token, jwtSecret);
+
+    req.user = await User.findById(verified.id).select("-password");
+    next();
+  } catch (error) {
+    res.status(401).json({ errors: ["Token inválido!"] });
+  }
+};
+
+module.exports = authGuard;
--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -2,16 +2,22 @@ const express = require("express");
 const router = express.Router();
 
 //Controller
-const { register, login } = require("../controllers/UserController");
+const {
+  register,
+  login,
+  getCurrentUser,
+} = require("../controllers/UserController");
 
 //mildewares
 const validate = require("../middlewares/handleValidation");
 const {
   userCreateValidation,
   loginValidation,
-} = require("../middlewares/UserValidation");
+} = require("../middlewares/userValidation");
+const authGuard = require("../middlewares/AuthGuard");
 //Routes
 router.post("/register", userCreateValidation(), validate, register);
 router.post("/login", loginValidation(), validate, login);
+router.get("/profile", authGuard, getCurrentUser);
 
 module.exports = router;

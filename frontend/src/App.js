d265cd58af428ee7877eafa18713aa639b15edf6
jjobJunior <54251905+jjobJunior@users.noreply.github.com>
2024-06-07 09:57:48 -0300

    Configurando React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,23 @@
-import './App.css';
+import "./App.css";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+
+//Pages
+import Home from "./pages/home/Home";
+
+//Router
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>Momentix</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
